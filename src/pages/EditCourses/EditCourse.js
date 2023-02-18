97ff9f916430c1058417759adf97d1daa2feee71
George Bjork <georgebjork@outlook.com>
2023-02-17 17:24:28 -0800

    now uses useFetch

--- src/pages/EditCourses/EditCourse.js
+++ src/pages/EditCourses/EditCourse.js
@@ -1,45 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import { useParams } from 'react-router-dom';
-import { getCoursesById } from '../../api/courseApi';
+import { useFetch } from "../../hooks/useFetch";
 import LoadingSpinner from "../../Components/LoadingSpinner";
 
 import EditTeeboxYardage from "../../Components/ScorecardEdit/EditTeeboxYardage"
 
 function EditCourse() {
 
-    const [course, setCourse] = useState();
     const { courseId } = useParams();
 
-    const [isLoading, setIsLoading] = useState(true);
-
-    useEffect(() => {
-        getCoursesById(courseId).then((response) => {
-            setCourse(response.data[0]);
-            setIsLoading(false);
-        })
-        .catch((err) => {
-            console.log("Error! " + err.message);
-        });
-    }, []);
+    const { isLoading, data, serverError } = useFetch(`http://localhost:5239/api/course/${courseId}`);
     
     return(
         <Container>
-            { isLoading ? <LoadingSpinner /> : (
-                <>
-                
-                    <h1 className="mt-5 mb-5">Edit {course.name} </h1> 
+            { isLoading && <LoadingSpinner /> }
 
-                    <h3> Teeboxes </h3>
-                    { course["teeboxes"].map(teebox => <EditTeeboxYardage teebox={teebox} />)}
+            {!isLoading && serverError ? (
+                <h3> Error </h3>
+            ) : (
+                data && <>
+                        <h1 className="mt-5 mb-5">Edit {data[0].name} </h1> 
+                        <h3> Teeboxes </h3>
+                        { data[0]["teeboxes"].map(teebox => <EditTeeboxYardage teebox={teebox} />) }
 
-                    <h3> Par </h3>
-                    <p> Par editing will go here </p>
-                    
-                    <h3> Handicap </h3>
-                    <p> Hanidcap editing will go here</p>
-                </>
-            )} 
+                        <h3> Par </h3>
+                        <p> Par editing will go here </p>
+                        
+                        <h3> Handicap </h3>
+                        <p> Hanidcap editing will go here</p>
+                    </>
+            )}
         </Container>
     )
 }
