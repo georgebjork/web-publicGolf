3ae61ee0a392218b20b1528f462e7032fa91a7de
George Bjork <georgebjork@outlook.com>
2023-02-18 17:47:33 -0800

    this should now allow for basic editing to happen

--- src/Components/EditTeeboxYardage.js
+++ src/Components/EditTeeboxYardage.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { Form, Table, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Table, Button } from "react-bootstrap";
 import { CourseHoles } from "./Scorecard.js"
 import "./EditTeeboxYardage.css"
 
 function EditTeeboxYardage({teebox}) {
 
-    const holes = teebox.holes
+    const [holes] = useState(teebox.holes);
+    const [newData, setNewData] = useState([]);
 
     const yardageElements = [];
 
@@ -13,7 +14,7 @@ function EditTeeboxYardage({teebox}) {
     yardageElements.push(<td>Yardage</td>);
 
     // All of the hole yardages
-    for(let i in holes) { yardageElements.push(<td><input type="text" className="hole"/></td>); }
+    for(let i in holes) { yardageElements.push(<td><input type="text" pattern="[0-9]*" placeholder={holes[i].yardage} id={holes[i].id} onBlur={handleInputChange} className="hole"/></td>); }
     
     // This will splice in the yardage out total
     yardageElements.splice(10, 0, <td>{teebox.yardageOut}</td>);
@@ -23,12 +24,30 @@ function EditTeeboxYardage({teebox}) {
     yardageElements.push(<td>{teebox.yardage}</td>);
     yardageElements.push(<td>{teebox.slope} / {teebox.rating}</td>);
 
+    function handleInputChange(e) {
+             
+        if(isNaN(e.target.value) || e.target.value === "") {
+            return;
+        }
+
+        // Find the hole and put into the newData array
+        const hole = holes.find(h => h.id === parseInt(e.target.id));
+        hole.yardage = parseInt(e.target.value);
+
+        // Set the new data
+        setNewData(prevData => [...prevData, hole]);
+    }
+
+    function handleSubmit() {
+        console.log(newData);
+    }
+
 
     return(
         <>  
             <div class="d-flex justify-content-between mt-5">
                 <h3>{teebox.name}</h3>
-                <Button variant="success"> Submit </Button>
+                <Button variant="success" onClick={handleSubmit}> Submit </Button>
             </div>
             
             <Table className="mt-3" striped responsive variant="dark" size="lg" style={{"text-align": "center", "vertical-align": "middle"}}>
